f1dfe3173babca0a42576f6c75fc7be71eae4dbd
Adam Detrick <akdetrick@gmail.com>
2017-06-28 11:23:11 -0400

    stub handlebars helpers

---
+++ scripts/util/handlebars/rgba.js
@@ -0,0 +1,8 @@
+const getRgbaString = require('../getRgbaString');
+
+/**
+ * @param {Array} colorArr
+ * @returns {String} rgba color string
+ */
+module.exports = colorArr =>
+	getRgbaString(colorArr);
