9084fc25e1de5547927b24164101b0980437ad32
Adam Detrick <akdetrick@gmail.com>
2017-06-27 21:36:08 -0400

    replace shitty log msg with somewhat useful ones

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,11 @@
 const StyleDictionary = require('style-dictionary').extend('./config.json');
 const TRANSFORMS = require('./transforms');
 
-console.warn(TRANSFORMS);
-
 TRANSFORMS
 	.forEach((t) => {
+		console.info(`Registering Transform: '${t.name}'`);
 		StyleDictionary.registerTransform(t);
 	});
 
+console.info('--------------------------\nBuilding distributions...');
 StyleDictionary.buildAllPlatforms();
