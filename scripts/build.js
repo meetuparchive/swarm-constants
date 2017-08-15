d230787d5a297c6c124cb5b33d5e940c237a3b28
Adam Detrick <akdetrick@gmail.com>
2017-08-15 17:06:04 -0400

    add commonJS format; use for js dist

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,18 @@
 const StyleDictionary = require('style-dictionary').extend('./config.json');
 const TRANSFORMS = require('./transforms');
+const FORMATS = require('./formats');
 const buildStaticDocs = require('./staticDoc').build;
 
 TRANSFORMS
 	.forEach((t) => {
 		console.info(`Registering Transform: '${t.name}'`);
 		StyleDictionary.registerTransform(t);
+	});
+
+FORMATS
+	.forEach((f) => {
+		console.info(`Registering Format: '${f.name}'`);
+		StyleDictionary.registerFormat(f);
 	});
 
 console.info('--------------------------\nBuilding distributions...');
