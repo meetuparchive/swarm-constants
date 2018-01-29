261575198531ecba57f60bb008db01506f7593ba
Adam Detrick <akdetrick@gmail.com>
2018-01-29 17:28:36 -0500

    add responsive properties platform

--- scripts/formats.js
+++ scripts/formats.js
@@ -1,13 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+
 /**
  * StyleDictionary custom formats
  * https://amzn.github.io/style-dictionary/formats_and_templates
  */
+
+
+// template for responsive custom properties format
+const templ_responsiveProperties = (ctx) => {
+	const bp = JSON.parse(
+			fs.readFileSync(
+				path.join(__dirname, '..', 'properties', 'layout', 'breakpoints.json')
+			)
+		);
+
+	const getCustomProperties = (size) =>
+		ctx.properties
+			.map(p => `--${p.path[1]}-${p.name}: ${p.value[size]};`)
+			.join('\n\t');
+
+	const getMQ = (size) =>
+		`only screen and (min-width: ${bp.layout.breakpoints[size].value})`;
+
+
+	return `
+:root {
+	${getCustomProperties('default')}
+}
+
+/* Medium breakpoint overrides */
+@media ${getMQ('m')} {
+	:root {
+		${getCustomProperties('atMedium')}
+	}
+}
+
+/* Large breakpoint overrides */
+@media ${getMQ('l')} {
+	:root {
+		${getCustomProperties('atLarge')}
+	}
+}
+`;
+};
 
 const dateHeader = () =>
 	  '/**\n' +
 		' * Do not edit directly\n' +
 		' * Generated on ' + new Date() + '\n' +
 		' */\n\n';
+
 
 const commonJS = {
 	name: 'javascript/commonJS',
@@ -26,6 +69,16 @@ const customProperties = {
 			.map(p => `\t--${p.name}: ${p.value};`)
 			.join('\n') +
 		'\n}'
+};
+
+const responsiveProperties = {
+	name: 'css/responsiveProperties',
+	formatter: (dictionary, platform) => dateHeader() +
+		templ_responsiveProperties({
+			properties: dictionary
+				.allProperties
+				.filter( p => p.path.includes('responsive') )
+		})
 };
 
 const colorAttributes = {
@@ -52,5 +105,6 @@ const colorAttributes = {
 module.exports = [
 	commonJS,
 	customProperties,
+	responsiveProperties,
 	colorAttributes
 ];
